f8976cddb127823c48ed37cc0e66e9f03e829c9f
Julio Sanquis <jsanquis@gmail.com>
2014-09-23 21:15:53 +0200

    Galería de Imágenes

---
+++ js/jquery_mixitup.js
@@ -0,0 +1,21 @@
+$(document).on('ready', function(){
+	
+	/*
+	--- Inicialización de Plugin de filtrado de elementos
+		Plugin: MixItUp - Sitio Web: https://mixitup.kunkalabs.com/	
+	*/
+	$('#contenedor').mixItUp();
+
+
+
+	/*
+	--- Inicialización de Plugin de Efectos MouseOver sobre Imágenes
+		Plugin: Adipoli - Sitio Web: https://cube3x.com/adipoli-jquery-image-hover-plugin/	
+	*/
+	$('.mix').adipoli({
+	    'startEffect' : 'normal',
+	    'hoverEffect' : 'popout'
+	});
+
+
+});
---
+++ js/jquery_plugins.js
@@ -0,0 +1,23 @@
+$(document).on('ready', function(){
+	
+	/*
+	--- Inicialización de Plugin de filtrado de elementos
+		Plugin: MixItUp - Sitio Web: https://mixitup.kunkalabs.com/	
+	*/
+	$('#contenedor').mixItUp();
+
+
+	/*
+	--- Inicialización de Plugin de Efectos MouseOver sobre Imágenes
+		Plugin: Adipoli - Sitio Web: https://cube3x.com/adipoli-jquery-image-hover-plugin/	
+	*/
+	$('article figure img').adipoli();
+
+
+	/*
+	--- Inicialización de Plugin de Efecto Lupa
+		Plugin: MagnifyMe - Sitio Web: http://www.codepunker.com/blog/magnify-me-jquery-image-magnifier-plugin	
+	
+    jQuery('.magnifyMe').magnifyMe('lupa');*/
+
+});
