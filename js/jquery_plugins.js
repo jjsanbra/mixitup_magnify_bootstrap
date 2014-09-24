1112b4e03d29023ed1e8947605d31277288f1f63
Julio Sanquis <jsanquis@gmail.com>
2014-09-24 21:13:20 +0200

    wip - Agregar textos a la ventana modal

--- js/jquery_plugins.js
+++ js/jquery_plugins.js
@@ -13,24 +13,59 @@ $(document).on('ready', function(){
 	*/
 	$('article figure img').adipoli();
 
+
 	/*
   	--- Inicialización de Plugin de Efecto Lupa
     	Plugin: MagnifyMe - Sitio Web: http://www.codepunker.com/blog/magnify-me-jquery-image-magnifier-plugin  
 	*/
     jQuery('.base').magnifyMe('lupa');
 
-
     /* Script para los botones de la ventana modal */
-    $('article a').on('click', function(){
+    	baseAttr = "";
+    	lupaAttr = "";
 
+    $('article').on('click', function(){
+    	baseAttr = $(this).find('a').attr('base');
+    	lupaAttr = $(this).find('a').attr('lupa');
+
+    	modalInfo(baseAttr);
+    }); 
+
+    var modalInfo = function(texto){
+    	
+    	//Variables para cargar las imágenes del MagnifyMe
+    	
+    	//Variables para el texto del párrafo en el modal
+    	var dep1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var dep2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var dep3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var dep4 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var nat1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var nat2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var nat3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var nat4 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var abs1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var abs2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var abs3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	var abs4 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, veniam magni error voluptatem, rem magnam iste blanditiis aliquam explicabo consequatur eum ducimus doloremque dolores est, molestiae tempora, ratione facilis asperiores!";
+    	
     	/*Selecciona todos los elementos con la clase
 		"base", con attr(par1, par2) le asigna un id con
 		la ruta "article>a" 
 		*/
 		//$('.base').attr('id','dep1');
-    	$('.base').attr('id',$(this).attr('base'));
-		$('.lupa').attr('id',$(this).attr('lupa'));    	
-    });
+		$('.modal-header h1').text($(this).find('img').attr('alt'));
+    	$('.base').attr('id', baseAttr);
+		$('.lupa').attr('id', lupaAttr);    	
+		
+		console.log($(this).find('img').attr('alt'));
+		//Condicional para cargar textos en la ventana modal
+		if(baseAttr == texto){
+			$('.modal-header p').text(texto);
+		} else {
+			$('.modal-header p').text("");
+		}
+    };
 
 
 });
